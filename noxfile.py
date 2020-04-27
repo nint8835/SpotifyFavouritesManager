599f799991251d7edf94612d5962b251d50a1489
Riley Flynn <riley@colabsoftware.com>
2020-04-27 20:26:37 -0230

    Pin nox versions, add requirements, add flake8 and mypy configs

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,22 @@
 import nox
+import nox.sessions
 
 PACKAGE_NAME = "spotifyfavouritesmanager"
 PACKAGE_FILES = [PACKAGE_NAME, "noxfile.py"]
 
 
 @nox.session
-def lint(session):
-    session.install("flake8", "black", "mypy")
+def lint(session: nox.sessions.Session) -> None:
+    session.install(
+        "flake8==3.7.9", "flake8-import-order==0.18.1", "black==19.10b0", "mypy==0.770"
+    )
     session.run("mypy", PACKAGE_NAME)
     session.run("flake8", *PACKAGE_FILES)
     session.run("black", "--check", *PACKAGE_FILES)
 
 
 @nox.session
-def format(session):
-    session.install("black", "isort")
+def format(session: nox.sessions.Session) -> None:
+    session.install("black==19.10b0", "isort==4.3.21")
     session.run("isort", "--recursive", *PACKAGE_FILES)
     session.run("black", *PACKAGE_FILES)
